a9f995cece75363a1a4799b59b348d1de2c6720b
David <guerre@dte.us.es>
2015-08-06 13:54:24 +0200

    modified functions read_gamepad_state and select_gamepad
    This will make it possible to select a joystick as master joystick i.e. a jooystick not associated to a player used to interact with the game front-end.

--- gamepad.c
+++ gamepad.c
@@ -41,6 +41,8 @@
 #include "simon.h"
 #endif
 
+#define VIRTUAL_GAMEPAD_ID (NUMBER_OF_INTERFACES-1)
+
 #define LED_CONFIG  (DDRD |= (1<<6))
 #define LED_OFF     (PORTD &= ~(1<<6))
 #define LED_ON      (PORTD |= (1<<6))
@@ -61,7 +63,6 @@
 #define SELECTOR_PORT PORTB
 #define SELECTOR_PINS PINB
 #define SELECTOR_PINS_MASK (((1<<NUMBER_OF_INTERFACES)-1)<<1)//player selection pins starting at 1
-#define select_gamepad(x) SELECTOR_PORT= SELECTOR_PINS_MASK & ~(1<<(x+1))
 
 #define is_active_pin(port,pin) (!(port&(1<<pin)))
 
@@ -88,21 +89,38 @@ uint8_t axis_value(uint8_t port,uint8_t decrement_pin, uint8_t increment_pin)
 	return 0x80;
 }
 
+volatile uint8_t scaned_gamepad;
+volatile uint8_t master_gamepad;
+
 void read_gamepad_state(uint8_t gamepad)
 {
-	gamepad_state[gamepad].buttons=~BUTTONS_PINS;
-
-	gamepad_state[gamepad].y_axis =	axis_value(DIRECTION_PINS,DOWN_PIN,UP_PIN);
-	gamepad_state[gamepad].x_axis = axis_value(DIRECTION_PINS,RIGHT_PIN,LEFT_PIN);
+	gamepad_state_t* state_to_modify= &gamepad_state[gamepad];
+	(*state_to_modify).buttons=~BUTTONS_PINS;
+	(*state_to_modify).y_axis =	axis_value(DIRECTION_PINS,DOWN_PIN,UP_PIN);
+	(*state_to_modify).x_axis = axis_value(DIRECTION_PINS,RIGHT_PIN,LEFT_PIN);
 }
+
+//#define select_gamepad(x) SELECTOR_PORT= SELECTOR_PINS_MASK & ~(1<<(x+1))
+
+void select_gamepad(uint8_t gamepad)
+{
+	uint8_t scanning_virtual_gamepad= (gamepad==VIRTUAL_GAMEPAD_ID);//requesting data for the virtual gamepad
+	uint8_t a_gamepad_has_been_selectes_as_master= (master_gamepad<VIRTUAL_GAMEPAD_ID);
+	uint8_t real_gamepad_to_read= scanning_virtual_gamepad? master_gamepad : gamepad;
+	uint8_t no_real_data_to_be_transmited=scanning_virtual_gamepad && !a_gamepad_has_been_selectes_as_master || !scanning_virtual_gamepad && gamepad==master_gamepad;
+	SELECTOR_PORT= no_real_data_to_be_transmited? SELECTOR_PINS_MASK : SELECTOR_PINS_MASK & ~(1<<(real_gamepad_to_read+1));
+}
+
+
+
 void usb_gamepad_reset_state(uint8_t gamepad)
 {
-	gamepad_state[gamepad].buttons=0;
-	gamepad_state[gamepad].y_axis =	0x80;
-	gamepad_state[gamepad].x_axis = 0x80;
+	gamepad_state_t* state_to_modify= &gamepad_state[gamepad];
+	(*state_to_modify).buttons=0;
+	(*state_to_modify).y_axis =	0x80;
+	(*state_to_modify).x_axis = 0x80;
 }
 
-volatile uint8_t selected_player;
 
 void configure_polling_interrupt(void)
 {
@@ -131,10 +149,11 @@ int main(void) {
 	SELECTOR_DDR=SELECTOR_PINS_MASK;//set as output
 	SELECTOR_PORT=SELECTOR_PINS_MASK;//no gamepad selected, deactivate pullups
 
-	for(selected_player=0;selected_player<NUMBER_OF_INTERFACES;selected_player++)
-		usb_gamepad_reset_state(selected_player); //players will be reported as idle by default
-	selected_player=0;
-	select_gamepad(selected_player);
+	for(scaned_gamepad=0;scaned_gamepad<NUMBER_OF_INTERFACES;scaned_gamepad++)
+		usb_gamepad_reset_state(scaned_gamepad); //players will be reported as idle by default
+	scaned_gamepad=0;
+	master_gamepad=VIRTUAL_GAMEPAD_ID;
+	select_gamepad(scaned_gamepad);
 
 	LED_CONFIG;
 	LED_ON; // power up led on startup for 1 sec
@@ -177,8 +196,8 @@ int main(void) {
 }
 
  ISR(TIMER0_COMPA_vect) {
- 	 read_gamepad_state(GAMEPAD_INTERFACE(selected_player));
- 	 usb_gamepad_send(GAMEPAD_INTERFACE(selected_player));
-	 selected_player=(selected_player+1)%(NUMBER_OF_INTERFACES-1);
-	 select_gamepad(selected_player);
+ 	 read_gamepad_state(GAMEPAD_INTERFACE(scaned_gamepad));
+ 	 usb_gamepad_send(GAMEPAD_INTERFACE(scaned_gamepad));
+	 scaned_gamepad=(scaned_gamepad+1)%NUMBER_OF_INTERFACES;
+	 select_gamepad(scaned_gamepad);
  }
