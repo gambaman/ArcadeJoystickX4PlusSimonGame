87b4b10bf4a0855e2edd9e0bb97932414e8ea16f
David <guerre@dte.us.es>
2015-08-06 23:36:35 +0200

    Had to change the read_gamepad_state and select_gamepad functions.
    Hopefully this will make possible to check the color light buttons properly.

--- gamepad.c
+++ gamepad.c
@@ -92,32 +92,60 @@ uint8_t axis_value(uint8_t port,uint8_t decrement_pin, uint8_t increment_pin)
 volatile uint8_t scaned_gamepad;
 volatile uint8_t master_gamepad;
 volatile uint8_t light_buttons_values;
+#define pressed_light_button(x) (light_buttons_values&(1<<x))
+//#define pressed_central_button pressed_light_button(VIRTUAL_GAMEPAD_ID)
+#define pressed_central_button (~CENTRAL_BUTTON_PINS & (1<<CENTRAL_BUTTON))
+#define turn_on_color_button_light(x) LIGHTS_PORT|= 1<<(x+1)
+#define turn_on_central_button_light LIGHTS_PORT|= 1
+#define turn_off_color_button_lights LIGHTS_PORT&= ~(((1<<(NUMBER_OF_INTERFACES))-1)<<1)
+#define turn_off_central_button_light LIGHTS_PORT&= ~1
+#define skip_bounces wait_for_miliseconds(50)
 
-void read_gamepad_state(uint8_t gamepad)
+void read_gamepad_state(void)
 {
-	gamepad_state_t* state_to_modify= &gamepad_state[gamepad];
-	(*state_to_modify).buttons=~BUTTONS_PINS;
-	(*state_to_modify).y_axis =	axis_value(DIRECTION_PINS,DOWN_PIN,UP_PIN);
-	(*state_to_modify).x_axis = axis_value(DIRECTION_PINS,RIGHT_PIN,LEFT_PIN);
+	uint8_t tmp_buttons,tmp_y_axis,tmp_x_axis;
+	tmp_buttons=~BUTTONS_PINS;
+	tmp_y_axis=axis_value(DIRECTION_PINS,DOWN_PIN,UP_PIN);
+	tmp_x_axis=axis_value(DIRECTION_PINS,RIGHT_PIN,LEFT_PIN);
+
+	if(scaned_gamepad!=VIRTUAL_GAMEPAD_ID)
+				if(scaned_gamepad!=master_gamepad)
+					{
+						gamepad_state[scaned_gamepad].buttons=tmp_buttons;
+						gamepad_state[scaned_gamepad].y_axis =tmp_y_axis;
+						gamepad_state[scaned_gamepad].x_axis =tmp_x_axis;
+					}
+				else
+					{
+						gamepad_state[VIRTUAL_GAMEPAD_ID].buttons=tmp_buttons;
+						gamepad_state[VIRTUAL_GAMEPAD_ID].y_axis =tmp_y_axis;
+						gamepad_state[VIRTUAL_GAMEPAD_ID].x_axis =tmp_x_axis;
+					}
+	else
+		{
+			gamepad_state[master_gamepad].buttons=tmp_buttons;
+			gamepad_state[master_gamepad].y_axis =tmp_y_axis;
+			gamepad_state[master_gamepad].x_axis =tmp_x_axis;
+		}
+
 }
 
-void select_gamepad(uint8_t gamepad)
+void select_gamepad(void)
 {
-	uint8_t scanning_virtual_gamepad= (gamepad==VIRTUAL_GAMEPAD_ID);//requesting data for the virtual gamepad
-	uint8_t a_gamepad_has_been_selectes_as_master= (master_gamepad<VIRTUAL_GAMEPAD_ID);
-	uint8_t real_gamepad_to_read= scanning_virtual_gamepad? master_gamepad : gamepad;
-	uint8_t no_real_data_to_be_transmited=scanning_virtual_gamepad && !a_gamepad_has_been_selectes_as_master || !scanning_virtual_gamepad && gamepad==master_gamepad;
-	SELECTOR_PORT= no_real_data_to_be_transmited? SELECTOR_PINS_MASK : SELECTOR_PINS_MASK & ~(1<<(real_gamepad_to_read+1));
+	// uint8_t scanning_virtual_gamepad= (gamepad==VIRTUAL_GAMEPAD_ID);//requesting data for the virtual gamepad
+	// uint8_t a_gamepad_has_been_selectes_as_master= (master_gamepad<VIRTUAL_GAMEPAD_ID);
+	// uint8_t real_gamepad_to_read= scanning_virtual_gamepad? master_gamepad : gamepad;
+	// uint8_t no_real_data_to_be_transmited=scanning_virtual_gamepad && !a_gamepad_has_been_selectes_as_master || !scanning_virtual_gamepad && gamepad==master_gamepad;
+	SELECTOR_PORT= SELECTOR_PINS_MASK & ~(1<<(scaned_gamepad+1));
 }
 
 
 
 void usb_gamepad_reset_state(uint8_t gamepad)
 {
-	gamepad_state_t* state_to_modify= &gamepad_state[gamepad];
-	(*state_to_modify).buttons=0;
-	(*state_to_modify).y_axis =	0x80;
-	(*state_to_modify).x_axis = 0x80;
+	gamepad_state[gamepad].buttons=0;
+	gamepad_state[gamepad].y_axis =	0x80;
+	gamepad_state[gamepad].x_axis = 0x80;
 }
 
 
@@ -151,8 +179,9 @@ int main(void) {
 	for(scaned_gamepad=0;scaned_gamepad<NUMBER_OF_INTERFACES;scaned_gamepad++)
 		usb_gamepad_reset_state(scaned_gamepad); //players will be reported as idle by default
 	scaned_gamepad=0;
-	master_gamepad=VIRTUAL_GAMEPAD_ID;
-	select_gamepad(scaned_gamepad);
+	master_gamepad=2;//VIRTUAL_GAMEPAD_ID;
+	light_buttons_values=0;
+	select_gamepad();
 
 	LED_CONFIG;
 	LED_ON; // power up led on startup for 1 sec
@@ -184,28 +213,52 @@ int main(void) {
 // }
 // 	nobeep;
 	LED_OFF;
-	while (1) {
+	while (1)
+	{
 		//
 		// if(is_active_pin(CENTRAL_BUTTON_PINS,CENTRAL_BUTTON))
 		// 	LIGHTS_PORT|= LIGHTS_PINS_MASK;// turn all of them on
 		// else
 		// 	LIGHTS_PORT&= ~LIGHTS_PINS_MASK;// turn all of them off
+		//LIGHTS_PORT=light_buttons_values<<1;//for debugging
+		//while(!light_buttons_values);//wait till a special light button is pressed
+		if(pressed_central_button)//master player change or Simon game request
+			{
+				turn_off_color_button_lights;
+				master_gamepad=VIRTUAL_GAMEPAD_ID;
+				turn_on_central_button_light;//for debugging
+				do
+				{
+					skip_bounces;
+					for(uint8_t i=0;i<VIRTUAL_GAMEPAD_ID;i++)
+					{
+									if(pressed_light_button(i))//select this button as master button
+									{
+											master_gamepad=i;
+											turn_off_color_button_lights;
+											turn_on_color_button_light(i);
+											turn_off_central_button_light;//for debugging
 
-		LIGHTS_PORT=light_buttons_values<<1;//for debugging
-	}
+									}
+						}
+				}while(pressed_central_button);
+			}
+	}/////////////////jarrrrrr
+	// else//insert coin request
+	// 	;
 }
 
 ISR(TIMER0_COMPA_vect)
 {
 	 uint8_t selected_light_button_mask=1<<scaned_gamepad;
- 	 read_gamepad_state(GAMEPAD_INTERFACE(scaned_gamepad));
+ 	 read_gamepad_state();
  	 usb_gamepad_send(GAMEPAD_INTERFACE(scaned_gamepad));
 	 light_buttons_values&=~selected_light_button_mask;
 	 if(
-		 scaned_gamepad!=VIRTUAL_GAMEPAD_ID && (~BUTTONS_PINS & (1<<7)) ||
-	 	 scaned_gamepad==VIRTUAL_GAMEPAD_ID && (~CENTRAL_BUTTON_PINS & (1<<CENTRAL_BUTTON))
+		 			scaned_gamepad!=VIRTUAL_GAMEPAD_ID && (~BUTTONS_PINS & (1<<7))
+		 //|| scaned_gamepad==VIRTUAL_GAMEPAD_ID && (~CENTRAL_BUTTON_PINS & (1<<CENTRAL_BUTTON))
 	 	)
 		 light_buttons_values|=selected_light_button_mask;
 	 scaned_gamepad=(scaned_gamepad+1)%NUMBER_OF_INTERFACES;
-	 select_gamepad(scaned_gamepad);
+	 select_gamepad();
  }
