b332c06bd96a3f2372006978762faa308f6ffd7c
David <guerre@dte.us.es>
2015-08-07 18:54:19 +0200

    fixed beep function

--- sound.c
+++ sound.c
@@ -13,8 +13,10 @@ void configure_beeper(void)
 
 void beep(uint16_t frequency)
 {
+	uint16_t ICR1_value=beeper_base_fequency/frequency;
 	TCCR1B=0; //disable timer
-	ICR1=beeper_base_fequency/frequency; // toggles when reach ICR1
+	ICR1H=ICR1_value>>8;
+	ICR1L=ICR1_value&255;
 	TCNT1=0;     // set timer0 counter initial value to 0
 	DDRB|=1<<7;
 	TCCR1B=_BV(WGM13)|_BV(WGM12)|5;//clear timer on compare match with ICR1|divide frequency by 64 (15625 Khz=>waveform of 7812 Khz)
